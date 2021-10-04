bc032d0ac0f38764d88d6bd7f3c2b62c52d4c460
Giap Nguyen <ngocgiapkt@gmail.com>
2021-10-04 15:36:44 +0700

    message

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,14 +1,33 @@
 // di chuyển modal dialog bằng modal header
 $('.modal-dialog').draggable({
-handle: ".modal-header"
+  handle: ".modal-header"
 });
 
 // ngăn ẩn dropdown khi click vào bên trong dropdown
 $(document).on('click', '.header_nav .dropdown-menu', function (e) {
-    e.stopPropagation();
-  });
+  e.stopPropagation();
+});
 
 // close accessibility menu
 $('.accessibility_close_btn').click(function () {
-    $('.accessibility_menu').toggleClass('show')
+  $('.accessibility_menu').toggleClass('show')
 })
+
+document.onscroll = function () {
+  // community section backgroundColor
+  var community = document.querySelector('.community_section')
+  if (community.offsetTop - window.scrollY < 400) {
+    community.style.backgroundColor = '#f7f7f7'
+  } else {
+    community.style.backgroundColor = '#a9d0d2'
+  }
+
+  // devices animation
+  var devices = document.querySelectorAll('.device_item')
+  for (var device of devices) {
+    let a = 400 / (device.offsetTop - window.scrollY);
+    let b = a > 1 ? 1 : a < 0.3 ? 0.3 : a;
+    device.style.transform = 'scale(' + b + ')';
+    device.style.opacity = a;
+  }
+}
